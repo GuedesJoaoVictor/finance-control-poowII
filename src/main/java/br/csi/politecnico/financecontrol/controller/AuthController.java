9a0b2e659292fd05b87b19ac628c754b767e6766
Gue10 <joaoguedes1404@gmail.com>
2025-09-15 17:58:50 -0300

    feat - Configurando security

---
+++ src/main/java/br/csi/politecnico/financecontrol/controller/AuthController.java
@@ -0,0 +1,7 @@
+package br.csi.politecnico.financecontrol.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+}
---
+++ src/main/java/br/csi/politecnico/financecontrol/security/JwtUtil.java
@@ -0,0 +1,54 @@
+package br.csi.politecnico.financecontrol.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key;
+    private final Long expiration;
+
+    private JwtUtil(
+            @Value("${finance.jwt.secret}") String secret,
+            @Value("${finance.jwt.expiration}") Long expiration
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.expiration = expiration;
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return parseClaims(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private Jws<Claims> parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
+}
