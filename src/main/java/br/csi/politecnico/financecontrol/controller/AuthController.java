0921bd4aaccfea6c2b4eae9897e3ad6c139e5777
Gue10 <joaoguedes1404@gmail.com>
2025-09-19 10:33:50 -0300

    feat - criando rotas de login e registro

--- src/main/java/br/csi/politecnico/financecontrol/controller/AuthController.java
+++ src/main/java/br/csi/politecnico/financecontrol/controller/AuthController.java
@@ -1,7 +1,41 @@
 package br.csi.politecnico.financecontrol.controller;
 
+import br.csi.politecnico.financecontrol.model.User;
+import br.csi.politecnico.financecontrol.repository.UserRepository;
+import br.csi.politecnico.financecontrol.security.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        return jwtUtil.generateToken(username);
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestParam User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
 }
