d93c6c77ce9c9b9eeca83c216837051ec3305bd5
Gue10 <joaoguedes1404@gmail.com>
2025-09-30 17:26:56 -0300

    style: adicionando swagger por anotações.

--- src/main/java/br/csi/politecnico/financecontrol/FinanceControlApplication.java
+++ src/main/java/br/csi/politecnico/financecontrol/FinanceControlApplication.java
@@ -1,8 +1,19 @@
 package br.csi.politecnico.financecontrol;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API de controle de finanças",
+				version = "1.0",
+				description = "Api para controlar as finanças",
+				contact = @Contact(name = "Gue10", email = "joaoguedes1404@gmail.com")
+		)
+)
 @SpringBootApplication
 public class FinanceControlApplication {
 
