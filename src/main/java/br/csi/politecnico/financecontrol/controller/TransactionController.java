ae0692c73ec78a42f3000288e7cbcc9633eaf56a
Gue10 <joaoguedes1404@gmail.com>
2025-10-25 18:51:10 -0300

    feat - Adicionando tag

--- src/main/java/br/csi/politecnico/financecontrol/controller/TransactionController.java
+++ src/main/java/br/csi/politecnico/financecontrol/controller/TransactionController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/transaction")
+@Tag(name = "Transaction", description = "Controller para todas as transações financeiras.")
 public class TransactionController {
 
     private final TransactionService transactionService;
