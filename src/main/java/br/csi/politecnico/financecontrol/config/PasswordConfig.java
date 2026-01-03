104a77eefcf53e7b085960c2bf43db40c9007764
Gue10 <joaoguedes1404@gmail.com>
2026-01-03 15:31:14 -0300

    fix - removing bean of passwordconfig

--- src/main/java/br/csi/politecnico/financecontrol/config/PasswordConfig.java
+++ src/main/java/br/csi/politecnico/financecontrol/config/PasswordConfig.java
@@ -1,6 +1,5 @@
 package br.csi.politecnico.financecontrol.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,7 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordConfig {
 
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
